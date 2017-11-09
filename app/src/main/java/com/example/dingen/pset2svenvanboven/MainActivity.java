207fea701a912d17faa2259c3ebfeb2ac7813906
SvenvBoven <sven_vboven@hotmail.com>
2017-11-09 14:03:32 +0100

    eerste ofzoooo

--- app/src/main/java/com/example/dingen/pset2svenvanboven/MainActivity.java
+++ app/src/main/java/com/example/dingen/pset2svenvanboven/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.dingen.pset2svenvanboven;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToSecond(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String text = editText.getText().toString();
+
+
+
+        Intent intent = new Intent(this,SecondActivity.class);
+        intent.putExtra("ourText", text);
+        startActivity(intent);
+        finish();
     }
 }
